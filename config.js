bb4b0ea4efae4a8bde8decedd5da38fb60b05298
truongvy-06 <120462320+truongvy-06@users.noreply.github.com>
2022-12-14 11:24:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+  version_check:
+    "https://raw.githubusercontent.com/HoangTran0410/extension/main/manifest.json",
+  source_code: "https://github.com/truongvy-06/Extension",
+};
